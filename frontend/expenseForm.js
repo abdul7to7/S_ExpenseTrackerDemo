1be99695023c44d5ac24d4730a85ddd2618deb01
unknown <abdul7to7@gmail.com>
2024-07-01 06:39:28 +0530

    optimised leaderboard

--- frontend/expenseForm.js
+++ frontend/expenseForm.js
@@ -1,28 +1,27 @@
 //Dom Loading expenses
 document.addEventListener("DOMContentLoaded", async () => {
   let data = await getAllExpenses();
-  if (data) {
+  if (data && data.expenses) {
     data.expenses.forEach((expense) => {
       addExpenseToUI(expense);
     });
   }
+  premiumUser(data.user.isPremium);
 });
 
 //adding an expense
-document
-  .getElementById("expenseForm")
-  ?.addEventListener("submit", async (e) => {
-    e.preventDefault();
-    console.log("button clicked");
-    const amount = document.getElementById("expenseAmount").value;
-    const description = document.getElementById("expenseDesc").value;
-    const category = document.getElementById("expenseCat").value;
-    let response = await postExpense({ amount, description, category });
-    if (response) {
-      console.log(response.message);
-      addExpenseToUI(response.expense);
-    }
-  });
+document.getElementById("expenseForm").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  console.log("button clicked");
+  const amount = document.getElementById("expenseAmount").value;
+  const description = document.getElementById("expenseDesc").value;
+  const category = document.getElementById("expenseCat").value;
+  let response = await postExpense({ amount, description, category });
+  if (response) {
+    console.log(response.message);
+    addExpenseToUI(response.expense);
+  }
+});
 
 //deleting an expense
 document.getElementById("expense-list").addEventListener("click", async (e) => {
@@ -42,17 +41,17 @@ document.getElementById("expense-list").addEventListener("click", async (e) => {
 document
   .getElementById("buy-premium-btn")
   .addEventListener("click", async (e) => {
-    let response = await fetch("http://localhost:4000/purchase/buymembership", {
+    let newOrder = await fetch("http://localhost:4000/purchase/buymembership", {
       headers: {
         "Content-Type": "application/json",
         token: localStorage.getItem("token"),
       },
     });
-    response = await response.json();
-    console.log(response);
+    newOrder = await newOrder.json();
+    console.log(newOrder);
     let options = {
-      key: response.key_id,
-      order_id: response.order.id,
+      key: newOrder.key_id,
+      order_id: newOrder.rzpOrder.id,
       handler: function (response) {
         fetch("http://localhost:4000/purchase/verifyPayment", {
           method: "POST",
@@ -61,25 +60,43 @@ document
             token: localStorage.getItem("token"),
           },
           body: JSON.stringify({
-            order_id: response.razorpay_order_id,
+            orderId: newOrder.orderId,
+            rzpOrderId: response.razorpay_order_id,
             payment_id: response.razorpay_payment_id,
             signature: response.razorpay_signature,
           }),
         })
           .then((response) => response.json())
           .then((data) => {
-            console.log(response);
+            console.log(data);
             alert(data.message);
+            localStorage.setItem("token", data.token);
+            premiumUser(data.user.isPremium);
           });
       },
     };
     let rzp_c = new Razorpay(options);
     rzp_c.open();
-    e.preventDefault();
     rzp_c.on("payment.failed", (response) => {
       console.log(response);
       alert("Something went wrong");
     });
+  });
+
+document
+  .getElementById("show-leaderboard-btn")
+  ?.addEventListener("click", async () => {
+    const leaderboardList = document.getElementById("leaderboard-list");
+    while (leaderboardList.firstChild) {
+      leaderboardList.removeChild(leaderboardList.firstChild);
+    }
+    let data = await getLeaderboard();
+    data.usersWithExpenses.forEach((item) => {
+      const li = document.createElement("li");
+      li.textContent = `${item.username} have total ${item.totalExpense} Expenses`;
+      leaderboardList.append(li);
+    });
+    console.log(data);
   });
 
 function addExpenseToUI(expense) {
@@ -158,4 +175,27 @@ async function deleteExpense(id) {
   } catch (e) {
     console.log(e);
   }
+}
+
+function premiumUser(isPremium) {
+  const premiumBtn = document.getElementById("buy-premium-btn");
+  const premiumStatus = document.getElementById("premium");
+
+  if (isPremium) {
+    premiumBtn.style.display = "none";
+    premiumStatus.style.display = "block";
+  } else {
+    premiumBtn.style.display = "block";
+    premiumStatus.style.display = "none";
+  }
+}
+
+async function getLeaderboard() {
+  let data = await fetch("http://localhost:4000/expense/leaderboard", {
+    headers: {
+      token: localStorage.getItem("token"),
+    },
+  });
+  data = await data.json();
+  return data;
 }
