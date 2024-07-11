696200d26ff4db852a8bfb578f40d2f6c9df3627
unknown <abdul7to7@gmail.com>
2024-07-11 08:54:08 +0530

    db update

--- backend/util/db.js
+++ backend/util/db.js
@@ -2,7 +2,7 @@ const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize("expense_demo", "root", "root", {
   dialect: "mysql",
-  host: "postgresql://expense_tracker_demo_sql_59b5_user:3ardFjmdwmeCTh12grIxR8FJuAkFPIgE@dpg-cq7kqfbv2p9s73c61rm0-a.singapore-postgres.render.com/expense_tracker_demo_sql_59b5",
+  host: "postgresql://expense_tracker_demo_sql_59b5_user:3ardFjmdwmeCTh12grIxR8FJuAkFPIgE@dpg-cq7kqfbv2p9s73c61rm0-a/expense_tracker_demo_sql_59b5",
 });
 
 module.exports = sequelize;
